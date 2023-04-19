8b32569e2e9648044adfa7ad50817bc5aba19ead
Sasha Afanasyev <afonia.marcii@gmail.com>
2023-04-19 09:57:52 +0300

    refactor: change avatar & text at nav  (Wednesday, April 19, 2023 6:57:52 AM)

--- src/components/ImageStart.vue
+++ src/components/ImageStart.vue
@@ -1,19 +1,19 @@
 <template>
   <div class="navigation">
     <router-link to="/" class="nav_avatar">
-      ><img
+      <img
         src="@/assets/image/photo_2022-12-11_10-29-09.jpg"
         alt="MyPhoto"
-        class="rounded-full"
+        class="nav_avatar_img"
     /></router-link>
 
     <div class="">
       <router-link to="/" class="">
-        <h1 class="font-bold">Afanasyev Aleksander</h1></router-link
+        <h1 class="avatar_name">Afanasyev Aleksander</h1></router-link
       >
-      <h2 class="">Frontend Developer</h2>
+      <h2 class="avatar_name_sublime">Frontend Developer</h2>
     </div>
-    <div class="">
+    <section class="nav_arr">
       <ul>
         <li><router-link to="/offer">OFFER</router-link></li>
         <li><router-link to="/portfolio">PORTFOLIO</router-link></li>
@@ -22,38 +22,41 @@
         <li><router-link to="/hobbies">MY HOBBIES</router-link></li>
         <li><router-link to="/contacts">CONTACT ME</router-link></li>
       </ul>
-    </div>
+    </section>
 
-    <div class="">
-      <div class="">Get in touch</div>
-      <div class="">
+    <section class="nav_arr_social">
+      <div class="label_nav_social">Get in touch:</div>
+      <div class="nav_arr_social_items">
         <a
           href="mailto:subwayserfer.job@gmail.com
 "
-          class="mr-1"
+          class=""
         >
-          <img class="w-8" src="@/assets/icon/icon-mail.png" alt="icon mail"
+          <img
+            class="nav_arr_social_items_el"
+            src="@/assets/icon/icon-mail.png"
+            alt="icon mail"
         /></a>
         <a href="https://www.linkedin.com/in/subway" target="_blank"
           ><img
-            class="w-7"
+            class="nav_arr_social_items_in"
             src="@/assets/icon/icon-linkedIn.png"
             alt="icon linkedIn"
         /></a>
         <a href="https://github.com/SubwaySerfer" target="_blank"
           ><img
-            class="w-9"
+            class="nav_arr_social_items_el"
             src="@/assets/icon/icon-github.png"
             alt="icon GitHub"
         /></a>
         <a href="https://www.instagram.com/afonia633/" target="_blank" class=""
           ><img
-            class=""
+            class="nav_arr_social_items_el"
             src="@/assets/icon/icon-insta.png"
             alt="icon Instagram"
         /></a>
       </div>
-    </div>
+    </section>
   </div>
 </template>
 
@@ -77,28 +80,61 @@ export default {
   display: flex;
 
   flex-direction: column;
+  gap: 2.5rem;
+  justify-content: center;
 }
 .nav_avatar {
   z-index: 1;
   width: 10rem;
+  align-self: center;
+  /* border: 2px solid black;
+  border-radius: 100px; */
+  box-sizing: border-box;
+  background-color: black;
+  border-radius: 25px;
+}
+.nav_avatar_img {
+  border-radius: 5px;
+  border: 2px solid black;
+  border-radius: 25px;
 }
 
-.font-bold {
+.avatar_name {
   font-weight: 700;
   font-size: 1.8rem;
+  text-align: center;
+}
+.avatar_name_sublime {
+  text-align: center;
+  font-size: 1.6rem;
+}
+
+.nav_arr {
+  align-self: flex-start;
+  padding-left: 3.3rem;
+  font-size: 1.4rem;
+}
+.nav_arr_social {
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+  margin-left: 3.3rem;
+}
+.label_nav_social {
+  font-size: 1.5rem;
+  font-weight: 500;
+}
+.nav_arr_social_items {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 0.5rem;
+}
+.nav_arr_social_items_el {
+  width: 2rem;
+}
+
+.nav_arr_social_items_in {
+  width: 1.6rem;
 }
 </style>
-
-<!-- w-44
-    row-start-1
-    row-end-2
-    col-start-1
-    justify-self-center
-    pt-6
-    col-end-4
-    pb-5
-    2xl:w-56
-    2xl:pt-9
-    xl:w-48
-    xl:pt-8
-    -- -->
